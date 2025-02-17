8e8703e4076b5e3ec214ba0287451b570c6415f0
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-16 22:45:19 -0800

    add tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return +(((fahrenheit - 32) * 5) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return +((celsius * 9) / 5 + 32).toFixed(1);
 };
 
+// round to one decimal point
+
+console.log(convertToCelsius(100), 37.8);
+console.log(convertToFahrenheit(73.2), 163.8);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
