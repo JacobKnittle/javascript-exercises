da68b4c7233eb531bf10714ba808c35bfad7de9a
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-16 18:53:17 -0800

    add 02_repeatstring solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numOfCopies) {
+    let repeatedString = '';
 
+    if (numOfCopies < 0){
+        return "ERROR"
+    }
+
+    for (let i = 0; i < numOfCopies; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
