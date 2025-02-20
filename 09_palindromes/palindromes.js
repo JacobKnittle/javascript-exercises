2aae5153354e6ed215a6fe6b67eab7502afb7880
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-19 18:33:32 -0800

    add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+// Write a function that determines whether or not a given string is a palindrome.
 
+// A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks:
+
+const palindromes = function (str) {
+  let punctuation = /[\.,?!]/g;
+  let alteredString = str
+    .toLowerCase()
+    .replaceAll(punctuation, "")
+    .replaceAll(" ", "");
+  return alteredString === alteredString.split("").reverse().join("");
 };
-
+console.log(palindromes("Leon Noel"));
 // Do not edit below this line
 module.exports = palindromes;
