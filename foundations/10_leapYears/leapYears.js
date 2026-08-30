05715272cc3e8b6f9ecc64ae9f18a87f24ebc469
JacobKnittle <jacobdknittle@gmail.com>
2026-08-30 10:30:47 -0700

    Add 10_leapYears solution

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap years are divisible by 400
+    if (year % 400 === 0) {
+        return true;
+        // leap years are not divisible by 100
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        return true
+        // leap year is divisible by four
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
