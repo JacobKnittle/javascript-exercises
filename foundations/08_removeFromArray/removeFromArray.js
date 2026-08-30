3115d5b89b58c04a17b7f42b31c0cdca861cc78a
JacobKnittle <jacobdknittle@gmail.com>
2026-08-30 08:03:14 -0700

    Add 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...removeAbles) {
+   
+    let filteredArray = []
+    // check if arr does not contain the removeable for each item
+    for (let i = 0; i < arr.length; i++) {
+        if (!removeAbles.includes(arr[i])) {
+            filteredArray.push(arr[i])
+        }
+    }
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
