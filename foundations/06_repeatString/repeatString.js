eb6461bd4d2a28dd4b1d70e66d18ced131176905
JacobKnittle <jacobdknittle@gmail.com>
2026-08-29 20:01:47 -0700

    Add 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeatedString = ''
+    if (num < 0) return 'ERROR'
+    // take the str input and concate it to the string the number of times
+    for (let i = 0; i < num; i++) {
+        repeatedString += str
+    }
+    return repeatedString
 
 };
+
+// repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
