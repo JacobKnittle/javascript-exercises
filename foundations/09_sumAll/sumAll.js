d58fa6bc6d715f4966193d544bb3f915d49fa714
JacobKnittle <jacobdknittle@gmail.com>
2026-08-30 08:24:16 -0700

    Add 09-SumAll-solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // take two positive ints
+    // if either of the parameters are not positive integers return 'ERROR'
+    if (isNaN(num1) || isNaN(num2) || num1 < 1 || num2 < 1 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR'
+    }
 
+    // check which parameter is bigger and assign them to the start and end numbers
+    let startNum;
+    let endNum;
+
+    if (num1 < num2) {
+        startNum = num1;
+        endNum = num2
+    } else {
+        startNum = num2
+        endNum = num1
+    }
+
+    // for loop is gonna start at smaller parameter of the two and its going to end at being equal to larger parameter
+    let sum = 0;
+
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    // return sum of every int between the two parameters
+    return sum;
 };
 
 // Do not edit below this line
