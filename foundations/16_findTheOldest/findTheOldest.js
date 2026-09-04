f07a2c8a412284b40b070f59cde31206db6caa48
JacobKnittle <jacobdknittle@gmail.com>
2026-09-04 11:28:45 -0700

    Add 16_findTheOldest solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = '';
+    people.map((person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = 2026
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+            oldestAge = person.yearOfDeath - person.yearOfBirth
+            oldestPerson = person.name
+        } 
+    })
+    return oldestPerson
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
