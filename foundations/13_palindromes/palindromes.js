dcbf610c9827f9ee8343aa305a5b303c0569ac03
JacobKnittle <jacobdknittle@gmail.com>
2026-09-04 10:27:15 -0700

    Add 13_palindromes solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase().replaceAll(/[.,\/#!$%' '\^&\*;:{}=\-_`~()]/g,"")
+    let reveredString = phrase.split('').reverse().join('');
 
+    return phrase === reveredString
+
+    // console.log(phrase)
+    // console.log(reveredString)
 };
-
+    
 // Do not edit below this line
 module.exports = palindromes;
+// console.log(palindromes('A car, a man, a maraca.'))
