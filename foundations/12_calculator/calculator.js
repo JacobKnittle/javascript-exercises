37e1199bd146f1b837284463f57eec46c52d3b8f
JacobKnittle <jacobdknittle@gmail.com>
2026-09-04 09:39:51 -0700

    Add Calculator solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a,b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for(let i = num; i > 0; i--) {
+    total *= i
+  } 
+  return total;
 };
 
 // Do not edit below this line
